7dc066a92f45a7f01b745388dfca9343d1814d3d
ozkavosh <augustoslva@hotmail.com>
2023-10-24 16:53:36 -0300

    Update header layout

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,5 +1,15 @@
+import { BiMoon } from 'react-icons/bi';
+import { HeaderContainer } from '../../components/HeaderContainer';
+import { HeaderTitle } from '../../components/HeaderTitle';
+import { DarkModeBtn } from '../../components/DarkModeBtn';
+
 export default function Header() {
   return (
-    <div>Header</div>
+    <HeaderContainer>
+      <HeaderTitle>Stellar Wallet</HeaderTitle>
+      <DarkModeBtn>
+        <BiMoon size={24}/>
+      </DarkModeBtn>
+    </HeaderContainer>
   )
 }
