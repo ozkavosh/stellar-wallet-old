df095995b8e026e53e5247a60af3d5418c39dc11
ozkavosh <augustoslva@hotmail.com>
2023-10-24 18:02:48 -0300

    Update home page with modal states

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { Button } from "../../components/Button";
 import { HomeContainer } from "../../components/HomeContainer";
+import LoginModal from "../../components/LoginModal";
+import CreateKeyModal from "../../components/CreateKeyModal";
 
 export default function Home() {
+  const [openLoginModal, setOpenLoginModal] = useState(false);
+  const [openCreateKeyModal, setCreateKeyModal] = useState(false);
+
   return (
     <HomeContainer>
-      <Button>Connect with a secret key</Button>
-      <Button>Generate key pair for a new account</Button>
+      <LoginModal openStatus={openLoginModal}/>
+      <CreateKeyModal openStatus={openCreateKeyModal}/>
+      <Button onClick={() => setOpenLoginModal(prev => !prev)}>Connect with a secret key</Button>
+      <Button onClick={() => setCreateKeyModal(prev => !prev)}>Generate key pair for a new account</Button>
     </HomeContainer>
   )
 }
