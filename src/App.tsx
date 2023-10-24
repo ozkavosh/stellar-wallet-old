6efd74ff0e6d0445adf1fae966d510d3b85b8de5
ozkavosh <augustoslva@hotmail.com>
2023-10-24 16:06:41 -0300

    Add routing scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes";
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
+import "./App.css";
 
 function App() {
-
   return (
-    <></>
-  )
+    <BrowserRouter>
+      <Header />
+        <Router />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
